7f2d47cfc5abda22dd7553c8d0973d4848ffc993
manishvbn <manishvbn@gmail.com>
2023-05-26 13:19:56 +0530

    Package Updates

--- Day2/2_external-modules/1_folder-watcher/app.js
+++ Day2/2_external-modules/1_folder-watcher/app.js
@@ -1,9 +1,21 @@
-const chokidar = require('chokidar');
+// const chokidar = require('chokidar');
 
-const watcher = chokidar.watch('./my-folder');
+// const watcher = chokidar.watch('./my-folder');
 
-watcher.on('add', (path) => { console.log(`File, ${path}, is created...`); });
-watcher.on('change', (path) => { console.log(`File, ${path}, is modified...`); });
-watcher.on('unlink', (path) => { console.log(`File, ${path}, is deleted...`); });
+// watcher.on('add', (path) => { console.log(`File, ${path}, is created...`); });
+// watcher.on('change', (path) => { console.log(`File, ${path}, is modified...`); });
+// watcher.on('unlink', (path) => { console.log(`File, ${path}, is deleted...`); });
 
-console.log("Chokidar is on duty....");
+// console.log("Chokidar is on duty....");
+
+// -------------------------------
+
+const _ = require('lodash');
+
+var employees = [{ id: 1, name: "Manish" },
+{ id: 2, name: "Abhijeet" },
+{ id: 3, name: "Ram" },
+{ id: 4, name: "Abhishek" },
+{ id: 5, name: "Ramakant" }];
+
+console.log(_.last(employees));
