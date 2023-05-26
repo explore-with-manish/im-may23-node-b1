1fd60e6b79a6da59451348190e22010a85bb6a1f
manishvbn <manishvbn@gmail.com>
2023-05-26 16:44:31 +0530

    Middlewares

--- Day2/2_external-modules/5_express-middlewares/app.js
+++ Day2/2_external-modules/5_express-middlewares/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const logger = require('morgan');
+const favicon = require('serve-favicon');
 
 // Create an Express Application Request Handler
 const app = express();
@@ -11,19 +13,46 @@ var employees = [{ id: 1, name: "Manish" },
 { id: 4, name: "Abhishek" },
 { id: 5, name: "Ramakant" }];
 
+// app.use((req, res, next) => {
+//     console.log('Request - Middleware One');
+//     next();
+//     console.log('Response - Middleware One');
+// });
+
+// app.use((req, res, next) => {
+//     console.log('Request - Middleware Two');
+//     next();
+//     console.log('Response - Middleware Two');
+// });
+
+// app.use((req, res, next) => {
+//     const stTime = new Date().getTime();
+//     next();
+//     const enTime = new Date().getTime();
+//     const tTime = enTime - stTime;
+//     console.log(`${req.url} - Total Time ${tTime} ms`);
+// });
+
+app.use(logger('dev'));
+app.use(favicon(__dirname + "/public/images/favicon.png"));
+
 app.get('/', (req, res, next) => {
-    res.render("index", { pageTitle: "Index View", sp:'<span>Hello</span>' });
+    console.log('Get Request Handler - Index');
+    res.render("index", { pageTitle: "Index View", sp: '<span>Hello</span>' });
 });
 
 app.get('/contact', (req, res, next) => {
+    console.log('Get Request Handler - Contact');
     res.render("contact", { pageTitle: "Contact View" });
 });
 
 app.get('/about', (req, res, next) => {
+    console.log('Get Request Handler - About');
     res.render("about", { pageTitle: "About View" });
 });
 
 app.get('/employees', (req, res, next) => {
+    console.log('Get Request Handler - Employees');
     res.render("employees", { pageTitle: "Employees View", empList: employees });
 });
 
