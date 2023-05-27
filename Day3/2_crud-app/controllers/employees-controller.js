e73607217cec8b4d40b7bba23807d856bc726fa0
manishvbn <manishvbn@gmail.com>
2023-05-27 11:44:02 +0530

    Create and Edit Done

--- Day3/2_crud-app/controllers/employees-controller.js
+++ Day3/2_crud-app/controllers/employees-controller.js
@@ -1,4 +1,4 @@
-const { getAllEmployees, getEmployee, deleteEmployee } = require("../data-access/employees-dal");
+const { getAllEmployees, getEmployee, deleteEmployee, insertEmployee, updateEmployee } = require("../data-access/employees-dal");
 
 exports.index = (req, res, next) => {
     res.render('employees/index', { pageTitle: 'Employees View', empList: getAllEmployees() });
@@ -7,6 +7,38 @@ exports.index = (req, res, next) => {
 exports.details = (req, res, next) => {
     var id = req.params.rId;
     res.render('employees/details', { pageTitle: 'Employee Details View', employee: getEmployee(id) });
+}
+
+exports.create_get = (req, res, next) => {
+    res.render('employees/create', { pageTitle: 'Employees Create View' });
+}
+
+exports.create_post = (req, res, next) => {
+    var { eid, ename } = req.body;
+    var employee = { id: parseInt(eid), name: ename };
+
+    if (insertEmployee(employee)) {
+        res.redirect('/employees');
+    } else {
+        res.render('employees/create', { pageTitle: 'Employees Create View' });
+    }
+}
+
+exports.edit_get = (req, res, next) => {
+    var id = req.params.rId;
+    res.render('employees/edit', { pageTitle: 'Employee Edit View', employee: getEmployee(id) });
+}
+
+exports.edit_post = (req, res, next) => {
+    var id = req.params.rId;
+    var { eid, ename } = req.body;
+    var employee = { id: parseInt(eid), name: ename };
+
+    if (updateEmployee(id, employee)) {
+        res.redirect('/employees');
+    } else {
+        res.render('employees/edit', { pageTitle: 'Employee Edit View', employee: getEmployee(id) });
+    }
 }
 
 exports.delete_get = (req, res, next) => {
