1d3d06d1c17d9408e589423345eca5e60181e940
manishvbn <manishvbn@gmail.com>
2023-06-02 15:30:33 +0530

    All Verbs Implemented

--- Day4/5_api-with-db-jquery-secured/controllers/employees-api-controller.js
+++ Day4/5_api-with-db-jquery-secured/controllers/employees-api-controller.js
@@ -1,9 +1,52 @@
-const { getAllEmployees } = require('../data-access/employees-dal');
+const { getAllEmployees, getEmployee, insertEmployee, updateEmployee, deleteEmployee } = require('../data-access/employees-dal');
 
 exports.getAll = (req, res, next) => {
     getAllEmployees().then(result => {
         res.status(200).json({ data: result, message: "Success, getting Employees" });
     }, eMsg => {
-        res.status(500).json({ data: result, message: "Error, getting Employees" });
+        res.status(500).json({ data: [], message: "Error, getting Employees" });
+    });
+}
+
+exports.get = (req, res, next) => {
+    var rid = req.params.rid;
+    getEmployee(rid).then(result => {
+        res.status(200).json({ data: result, message: "Success, getting Employee" });
+    }, eMsg => {
+        res.status(500).json({ data: null, message: "Error, getting Employee" });
+    });
+}
+
+exports.create = (req, res, next) => {
+    var { eid, ename } = req.body;
+    var employee = { id: parseInt(eid), name: ename };
+
+    insertEmployee(employee).then(result => {
+        res.status(201).json({ data: result, message: "Success, creating Employee" });
+    }, eMsg => {
+        res.status(500).json({ data: { eid, ename }, message: "Error, creating Employee" });
+    });
+}
+
+exports.update = (req, res, next) => {
+    var rid = req.params.rid;
+
+    var { eid, ename } = req.body;
+    var employee = { id: parseInt(eid), name: ename };
+
+    updateEmployee(rid, employee).then(result => {
+        res.status(200).json({ data: result, message: "Success, updating Employee" });
+    }, eMsg => {
+        res.status(500).json({ data: { eid, ename }, message: "Error, updating Employee" });
+    });
+}
+
+exports.delete = (req, res, next) => {
+    var rid = req.params.rid;
+
+    deleteEmployee(rid).then(result => {
+        res.status(200).json({ data: rid, message: "Success, deleting Employee" });
+    }, eMsg => {
+        res.status(500).json({ data: rid, message: "Error, deleting Employee" });
     });
 }
--- Day4/5_api-with-db-jquery-secured/routes/employees-api.js
+++ Day4/5_api-with-db-jquery-secured/routes/employees-api.js
@@ -13,4 +13,16 @@ router.use(validateToken);
 // GET - /api/employees (Get All Employees)
 router.get("/", empApiCtrl.getAll);
 
+// GET - /api/employees/hasjkhdkjasdhj (Get Employee by Id)
+router.get("/:rid", empApiCtrl.get);
+
+// POST - /api/employees + Data in Body (Create a new Employee)
+router.post("/", empApiCtrl.create);
+
+// PUT - /api/employees/hasjkhdkjasdhj + Data in Body (Update Employee by Id)
+router.put("/:rid", empApiCtrl.update);
+
+// DELETE - /api/employees/hasjkhdkjasdhj (Delete Employee by Id)
+router.delete("/:rid", empApiCtrl.delete);
+
 module.exports = router;
