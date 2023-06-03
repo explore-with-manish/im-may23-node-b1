a08df0a90744f6a2da294725cf8c4153a73939ac
manishvbn <manishvbn@gmail.com>
2023-06-03 14:31:54 +0530

    Loaders & Mode

--- Day5/5_configuring-webpack/webpack.config.js
+++ Day5/5_configuring-webpack/webpack.config.js
@@ -12,12 +12,28 @@
 
 const path = require("path");
 
-module.exports = {
+let production = process.env.NODE_ENV === "production";
+
+let config = {
     entry: ["./src/index.js", "./src/home.js"],
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, 'dist'),
         // clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            }
+        ]
     },
     devtool: 'inline-source-map',
     mode: "development",
@@ -25,3 +41,10 @@ module.exports = {
         static: "./dist"
     }
 };
+
+if (production) {
+    config.devtool = 'source-map';
+    config.mode = "production";
+}
+
+module.exports = config;
