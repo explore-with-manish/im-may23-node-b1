6e0faf24599617eb838fcc40464cbe0374c579b7
manishvbn <manishvbn@gmail.com>
2023-06-03 14:00:14 +0530

    Webpack Configuration

---
+++ Day5/5_configuring-webpack/webpack.config.js
@@ -0,0 +1,27 @@
+// const path = require("path");
+
+// module.exports = {
+//     entry: "./src/index.js",
+//     output: {
+//         filename: "code.js",
+//         path: path.resolve(__dirname, 'build')
+//     }
+// };
+
+// ------------------------------
+
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, 'dist'),
+        // clean: true
+    },
+    devtool: 'inline-source-map',
+    mode: "development",
+    devServer: {
+        static: "./dist"
+    }
+};
